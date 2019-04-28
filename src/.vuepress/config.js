0f947535cc87128c5e2eaaec74a9d395e4930a21
GaoSir <gaojj@cntest.com>
2019-04-28 17:44:59 +0800

    2019.4.19 首次提交

---
+++ src/.vuepress/config.js
@@ -0,0 +1,85 @@
+module.exports = {
+    theme: 'meteorlxy',
+    title: '❤GaoSir 的个人博客',
+    description: 'GaoSir 的个人博客',
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/images/favicon.ico'
+        }]
+    ],
+
+    // 主题设置
+    themeConfig: {
+        // 主题语言
+        lang: Object.assign(require('vuepress-theme-meteorlxy/lib/langs/zh-CN'), {
+            prevPost: '上一篇文章',
+            nextPost: '下一篇文章',
+            notFound: '哎呀，页面跑丢了！'
+        }),
+
+        // 个人信息设置
+        personalInfo: {
+            nickname: 'GaoSir',
+            description: '你若想要得到，就别只是期望。<br/> 人生短暂，经不起等待。',
+            email: 'gaosir_801@163.com',
+            location: '深圳',
+            avatar: '/images/tx.jpg',
+            sns: {
+                github: {
+                    account: 'Gaosirs',
+                    link: 'https://github.com/gaosirs',
+                }
+            }
+        },
+
+        // 头部设置
+        header: {
+            background: {
+                useGeo: true,
+            },
+            showTitle: true,
+        },
+
+        // 帖子的上次更新时间
+        lastUpdated: true,
+
+        // 导航栏设置
+        nav: [{
+                text: '首页',
+                link: '/',
+                exact: true
+            },
+            {
+                text: '文章',
+                link: '/posts/',
+                exact: false
+            },
+            {
+                text: '博客园',
+                link: 'https://www.cnblogs.com/gaosirs',
+                exact: false
+            }
+        ],
+
+        // 评论设置
+        comments: {
+            platform: 'github',
+            owner: 'GaoSirs',
+            repo: 'Vuepress-Blog',
+            clientId: '02a7a5006e86d2573e7e',
+            clientSecret: '245cc5034afc54d1d99a93051d9092c3465bf358',
+            autoCreateIssue: process.env.NODE_ENV !== 'development'
+        },
+
+        // 分页配置
+        pagination: {
+            perPage: 5
+        }
+    }
+}
---
+++ src/_posts/first.md
@@ -0,0 +1,18 @@
+---
+category: 其他
+tags:
+  - 其他
+date: 2019-04-28
+title: 第一篇文章!
+vssue-title: 第一篇文章！
+---
+
+## 前言
+This is my first post.
+
+## 具体
+The content above `more` is the excerpt, which will be displayed in the posts list.
+
+## 其他
+
+The content below `more` will only be displayed when viewing this post, and will not be displayed in the posts list.
